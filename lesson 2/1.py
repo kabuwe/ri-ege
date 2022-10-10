3c2dbe51a0d5448e96b09ed8c7abcd29d53e70d8
Кидрей <indusyatina.lool@gmail.com>
2022-10-10 21:52:00 +0300

    hello world

--- lesson 2/1.py
+++ lesson 2/1.py
@@ -1,6 +1,7 @@
-print('whats your name?')
-print('how old are you?')
-print('where are you live?')
-print('this is', input())
+print('whats your name?')#вывожу вопросы на экран
+print('this is', input())#вывожу ответы на экран
+print('how old are you?')#вывожу вопросы на экран
 print('it is', input())
+print('where are you live?')#вывожу вопросы на экран
 print('(S)hi live in', input())
+
