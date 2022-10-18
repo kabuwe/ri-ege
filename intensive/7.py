cd69ee19762a4f4f335e291e367805c0bdf99d47
Кидрей <indusyatina.lool@gmail.com>
2022-10-18 21:43:29 +0300

    hello world

---
+++ intensive/7.py
@@ -0,0 +1,19 @@
+a=int(input())
+if 1<=a<=4:
+    print(1)
+if 5<=a<=8:
+    print(2)
+if 9<=a<=12:
+    print(3)
+if 13<=a<=16:
+    print(4)
+if 17<=a<=20:
+    print(5)
+if 21<=a<=24:
+    print(6)
+if 25<=a<=28:
+    print(7)
+if 29<=a<=32:
+    print(8)
+if 33<=a<=36:
+    print(9)
