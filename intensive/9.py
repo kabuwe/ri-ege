53c515c5111a620d017696dde19f19fb3b65ee35
Кидрей <indusyatina.lool@gmail.com>
2022-10-20 21:27:33 +0300

    hello world

--- intensive/9.py
+++ intensive/9.py
@@ -1,5 +1,5 @@
-a, b, c, d = int(input()), int(input()), int(input()), int(input())
-if a == c or b == d:
+stolb1, str1, stolb2, str2 = int(input()), int(input()), int(input()), int(input())
+if stolb1 == stolb2 or str1 == str2:
     print('YES')
 else:
     print('NO')
